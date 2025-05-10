eae24e544d617324674e6c45f2d71570075563d7
amangupta201 <113130485+amangupta201@users.noreply.github.com>
2025-05-10 13:02:07 +0530

    specified direct path of .env file

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-#load .env variables
-load_dotenv()
+load_dotenv(dotenv_path="/home/ec2-user/.env")
 
-OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
-OPENAI_MODEL=os.getenv("OPENAI_MODEL")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_MODEL = os.getenv("OPENAI_MODEL")
